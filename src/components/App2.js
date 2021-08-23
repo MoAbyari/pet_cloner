ddfe2122adc0ba3a482b6532e78c5e8da4cdb33c
Pat <patmifsud@me.com>
2021-08-23 12:52:25 +1000

    pushing to firebase

--- src/components/App2.js
+++ src/components/App2.js
@@ -1,16 +1,39 @@
 import './App.css';
 import { db } from '../services/firebase.js'
 
-function _submitHandler(){
-   console.log('hello')
+const database = db.collection("Users");
+
+class Signup {
+   constructor(
+      firstName='Lay', 
+      lastName='Smith', 
+      email='email@address.com', 
+      state='New South Wales', 
+      dog=true, 
+      cat=false) 
+      {
+         this.firstName = firstName;
+         this.lastName = lastName;
+         this.email = email;
+         this.state = state;
+         this.dog = dog;
+         this.cat = cat;
+   }
 }
 
-function App() {
+function _submitHandler(){
+   const formEntry = new Signup (
+      ("May", "Jones","email@address.com","New South Wales", false, true)
+   )
+   database.doc().set({...formEntry});
+}
+
+function App2() {
   return (
     <div className="App2">
-       <button onClick={_submitHandler}></button>
+       <button onClick={_submitHandler}>Test posting</button>
     </div>
   );
 }
 
-export default App;
+export default App2;
