01ea928ed0b1477ef087ff72cf4a36dca0d77574
Pat <patmifsud@me.com>
2021-08-23 11:56:52 +1000

    set up seperate file for firebase

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { db } from '../services/firebase.js'
+
+function App() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/firebase.js
@@ -0,0 +1,10 @@
+
+function db() {
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default db;
